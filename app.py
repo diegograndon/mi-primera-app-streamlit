5d867985d7c9314eb1a786055ec68b11ce5fb632
diegograndon <diego.grandon@csi.cl>
2025-10-27 11:07:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import streamlit as st
 # Título de la app
 st.title("¡Mi primera app!")
 
-if st.button("Click aquí"): st.write("¡Botón presionado!")
+st.success("Mensaje de éxito ✅") 
+st.error("Mensaje de error ❌") 
+st.warning("Advertencia ⚠️") 
+st.info("Información ℹ️") 
 
 # Texto simple
 st.write("Hola, soy [TU NOMBRE] y esta es mi primera aplicación con Streamlit.")
